fe0c9b826edf4a9948f12401a67fed172eda1b86
Beloux-Beloux <rasamoelinamampitarivo@gmail.com>
2026-07-02 15:52:41 +0300

    Fix cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:4173'],
+  origin: '*',
   credentials: true,
 }));
 app.use(express.json());
